49c3aea3d0c367018b8428abcca95e9d6f487979
noppefoxwolf <noppelabs@gmail.com>
2019-08-22 02:34:33 +0900

    Update CGImage+Extensions.swift

--- WarpGeometryFilter/Classes/CGImage+Extensions.swift
+++ WarpGeometryFilter/Classes/CGImage+Extensions.swift
@@ -10,23 +10,16 @@ import CoreGraphics
 extension CGImage {
   static func make(cvPixelBuffer: CVPixelBuffer, colorspace: CGColorSpace = CGColorSpaceCreateDeviceRGB()) -> CGImage? {
     CVPixelBufferLockBaseAddress(cvPixelBuffer, .init(rawValue: 0))
-    guard let sourceBaseAddr = CVPixelBufferGetBaseAddress(cvPixelBuffer) else {
+    defer {
       CVPixelBufferUnlockBaseAddress(cvPixelBuffer, .init(rawValue: 0))
-      return nil
     }
+    guard let sourceBaseAddr = CVPixelBufferGetBaseAddress(cvPixelBuffer) else { return nil }
     let height: Int = CVPixelBufferGetHeight(cvPixelBuffer)
     let width: Int = CVPixelBufferGetWidth(cvPixelBuffer)
     let bytesPerRow: Int = CVPixelBufferGetBytesPerRow(cvPixelBuffer)
     let bitmapInfo: CGBitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.noneSkipFirst.rawValue | CGBitmapInfo.byteOrder32Little.rawValue)
     var cvPixelBuffer = cvPixelBuffer
-    guard let provider = CGDataProvider(dataInfo: &cvPixelBuffer, data: sourceBaseAddr, size: bytesPerRow * height, releaseData: { (rawPixelBuffer, data, size) in
-      if let usedPixelBuffer = rawPixelBuffer?.bindMemory(to: CVPixelBuffer.self, capacity: size) {
-        CVPixelBufferUnlockBaseAddress(usedPixelBuffer.pointee, .init(rawValue: 0))
-      }
-    }) else {
-      CVPixelBufferUnlockBaseAddress(cvPixelBuffer, .init(rawValue: 0))
-      return nil
-    }
+    guard let provider = CGDataProvider(dataInfo: &cvPixelBuffer, data: sourceBaseAddr, size: bytesPerRow * height, releaseData: { (_, _, _) in }) else { return nil }
     return CGImage(
       width: width,
       height: height,
